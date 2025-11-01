bbba319b3f36095500fb4043ecc06cbb43b902e5
joseedusarmento <j.eduardo.sarmento10@gmail.com>
2025-11-01 01:05:17 -0300

    Correção de erros e ajuste de Templates

--- main/urls.py
+++ main/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name='index'),
     path('aluno/', AlunoListView.as_view(), name='alunos_list'),
-    path('aluno/<int:id>/',AlunoDetailView.as_view(), name='aluno_detail'),
+    path('aluno/<int:pk>/',AlunoDetailView.as_view(), name='aluno_detail'),
     path('aluno/novo/', AlunoCreateView.as_view(), name='aluno_create'),
-    path('aluno/<int:id>/editar/', AlunoUpdateView.as_view(), name='aluno_update'),
-    path('aluno/<int:id>/excluir/', AlunoDeleteView.as_view(), name='aluno_delete'),
+    path('aluno/<int:pk>/editar/', AlunoUpdateView.as_view(), name='aluno_update'),
+    path('aluno/<int:pk>/excluir/', AlunoDeleteView.as_view(), name='aluno_delete'),
     path('curso/listar/', curso_listar, name='curso_listar'),
     path('curso/criar/', curso_criar, name='curso_criar'),
     path('curso/remover/<int:id>/',curso_remover,name='curso_remover'),
@@ -45,5 +45,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
